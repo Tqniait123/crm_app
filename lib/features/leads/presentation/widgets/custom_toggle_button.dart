ed7b6a65e0a5b854fcad6e8b8a36a651458d53e3
Eldengawy <eldengaawy@gmail.com>
2025-09-22 15:45:14 +0300

    refactor(leads): simplify custom toggle button widget
    remove unused animation code and add haptic feedback
    clean up widget structure by removing advanced toggle button variant

--- lib/features/leads/presentation/widgets/custom_toggle_button.dart
+++ lib/features/leads/presentation/widgets/custom_toggle_button.dart
@@ -2,6 +2,7 @@ import 'package:crm_app/core/extensions/string_to_icon.dart';
 import 'package:crm_app/core/static/icons.dart';
 import 'package:crm_app/core/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CustomToggleButton extends StatefulWidget {
   final bool isSelected;
@@ -31,7 +32,7 @@ class CustomToggleButton extends StatefulWidget {
     this.unselectedItemColor = Colors.grey,
     this.borderRadius = 10.0,
     this.padding = const EdgeInsets.all(4),
-    this.itemPadding = const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+    this.itemPadding = const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
     this.spacing = 10.0,
   });
 
@@ -62,13 +63,11 @@ class CustomToggleButton extends StatefulWidget {
 
 class _CustomToggleButtonState extends State<CustomToggleButton> with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
-  late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
     _animationController = AnimationController(duration: widget.animationDuration, vsync: this);
-    _animation = CurvedAnimation(parent: _animationController, curve: Curves.easeInOut);
 
     if (widget.isSelected) {
       _animationController.forward();
@@ -95,6 +94,7 @@ class _CustomToggleButtonState extends State<CustomToggleButton> with SingleTick
 
   void _handleTap(bool isFirst) {
     if ((isFirst && !widget.isSelected) || (!isFirst && widget.isSelected)) {
+      HapticFeedback.lightImpact();
       widget.onToggle(!widget.isSelected);
     }
   }
@@ -137,168 +137,15 @@ class _CustomToggleButtonState extends State<CustomToggleButton> with SingleTick
   }) {
     return GestureDetector(
       onTap: onTap,
-      child: AnimatedBuilder(
-        animation: _animation,
-        builder: (context, child) {
-          final animationValue = isFirst ? _animation.value : (1 - _animation.value);
-
-          return AnimatedContainer(
-            duration: widget.animationDuration,
-            curve: Curves.easeInOut,
-            padding: widget.itemPadding,
-            decoration: BoxDecoration(
-              color: isSelected ? widget.selectedBackgroundColor : Colors.transparent,
-              borderRadius: BorderRadius.circular(widget.borderRadius),
-              boxShadow: isSelected
-                  ? [BoxShadow(color: Colors.black.withOpacity(0.05), blurRadius: 4, offset: const Offset(0, 2))]
-                  : null,
-            ),
-            child: Transform.scale(
-              scale: 0.9 + (animationValue * 0.1), // Subtle scale animation
-              child: icon.icon(color: isSelected ? widget.selectedItemColor : widget.unselectedItemColor),
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
-// Alternative version with more customization options
-class AdvancedCustomToggleButton<T> extends StatefulWidget {
-  final T selectedValue;
-  final List<ToggleItem<T>> items;
-  final Function(T) onChanged;
-  final Duration animationDuration;
-  final Color backgroundColor;
-  final Color selectedBackgroundColor;
-  final Color selectedItemColor;
-  final Color unselectedItemColor;
-  final double borderRadius;
-  final EdgeInsetsGeometry padding;
-  final EdgeInsetsGeometry itemPadding;
-  final double spacing;
-  final bool showShadow;
-
-  const AdvancedCustomToggleButton({
-    super.key,
-    required this.selectedValue,
-    required this.items,
-    required this.onChanged,
-    this.animationDuration = const Duration(milliseconds: 200),
-    this.backgroundColor = const Color(0xffF2F2F2),
-    this.selectedBackgroundColor = Colors.white,
-    this.selectedItemColor = Colors.black,
-    this.unselectedItemColor = Colors.grey,
-    this.borderRadius = 10.0,
-    this.padding = const EdgeInsets.all(4),
-    this.itemPadding = const EdgeInsets.symmetric(horizontal: 13, vertical: 4),
-    this.spacing = 10.0,
-    this.showShadow = true,
-  });
-
-  @override
-  State<AdvancedCustomToggleButton<T>> createState() => _AdvancedCustomToggleButtonState<T>();
-}
-
-class _AdvancedCustomToggleButtonState<T> extends State<AdvancedCustomToggleButton<T>> with TickerProviderStateMixin {
-  late List<AnimationController> _controllers;
-  late List<Animation<double>> _animations;
-
-  @override
-  void initState() {
-    super.initState();
-    _controllers = List.generate(
-      widget.items.length,
-      (index) => AnimationController(duration: widget.animationDuration, vsync: this),
-    );
-    _animations = _controllers
-        .map((controller) => CurvedAnimation(parent: controller, curve: Curves.easeInOut))
-        .toList();
-
-    _updateAnimations();
-  }
-
-  void _updateAnimations() {
-    for (int i = 0; i < widget.items.length; i++) {
-      if (widget.items[i].value == widget.selectedValue) {
-        _controllers[i].forward();
-      } else {
-        _controllers[i].reverse();
-      }
-    }
-  }
-
-  @override
-  void didUpdateWidget(AdvancedCustomToggleButton<T> oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.selectedValue != widget.selectedValue) {
-      _updateAnimations();
-    }
-  }
-
-  @override
-  void dispose() {
-    for (var controller in _controllers) {
-      controller.dispose();
-    }
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: widget.padding,
-      decoration: BoxDecoration(
-        color: widget.backgroundColor,
-        borderRadius: BorderRadius.circular(widget.borderRadius),
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: widget.items.asMap().entries.map((entry) {
-          final index = entry.key;
-          final item = entry.value;
-          final isSelected = item.value == widget.selectedValue;
-
-          return Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              if (index > 0) SizedBox(width: widget.spacing),
-              GestureDetector(
-                onTap: () => widget.onChanged(item.value),
-                child: AnimatedBuilder(
-                  animation: _animations[index],
-                  builder: (context, child) {
-                    return AnimatedContainer(
-                      duration: widget.animationDuration,
-                      curve: Curves.easeInOut,
-                      padding: widget.itemPadding,
-                      decoration: BoxDecoration(
-                        color: isSelected ? widget.selectedBackgroundColor : Colors.transparent,
-                        borderRadius: BorderRadius.circular(widget.borderRadius),
-                        boxShadow: isSelected && widget.showShadow
-                            ? [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.05),
-                                  blurRadius: 4,
-                                  offset: const Offset(0, 2),
-                                ),
-                              ]
-                            : null,
-                      ),
-                      child: Transform.scale(
-                        scale: 0.9 + (_animations[index].value * 0.1),
-                        child:
-                            item.child ??
-                            item.icon.icon(color: isSelected ? widget.selectedItemColor : widget.unselectedItemColor),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
-          );
-        }).toList(),
+      child: AnimatedContainer(
+        duration: widget.animationDuration,
+        curve: Curves.easeInOut,
+        padding: widget.itemPadding,
+        decoration: BoxDecoration(
+          color: isSelected ? widget.selectedBackgroundColor : widget.backgroundColor,
+          borderRadius: BorderRadius.circular(widget.borderRadius),
+        ),
+        child: icon.icon(color: isSelected ? widget.selectedItemColor : widget.unselectedItemColor),
       ),
     );
   }
